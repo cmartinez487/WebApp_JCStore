5b6c218cd4ee86ccca845178c4ee2f380aa4de80
Carlos Martinez <cmartinez487@gmail.com>
2022-07-16 17:12:43 -0500

    Commit for new proyect
    add start structure for new proyect

---
+++ WebApp_JCStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp_JCStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
